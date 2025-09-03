129b8d01d37a752abaca374272bc6b4c81157cdc
TatyanaEn <enatskayatatyana@yandex.ru>
2025-09-03 22:55:58 +0300

    Исправление замечаний

--- gateway/src/main/java/ru/practicum/shareit/constants/AppConstants.java
+++ gateway/src/main/java/ru/practicum/shareit/constants/AppConstants.java
@@ -1,7 +1,8 @@
 package ru.practicum.shareit.constants;
 
 public final class AppConstants {
-    private AppConstants() {}
+    private AppConstants() {
+    }
 
     public static final String USER_ID_FIELD = "X-Sharer-User-Id";
 }
